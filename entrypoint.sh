6c30aabb37ede2a12e2bc6d286faa5e8f2d6a2c9
Ankit Mishra <ankit.lex96@gmail.com>
2024-02-08 22:51:43 +0530

    production config added

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,12 @@ echo 'Running migrations...'
 python manage.py makemigrations
 python manage.py migrate
 
+# Create a directory for collecting static files
+RUN mkdir -p /code/staticfiles
+
+# Set STATIC_ROOT environment variable to the newly created directory
+ENV STATIC_ROOT /code/staticfiles
+
 echo 'Collecting static files...'
 python manage.py collectstatic --no-input
 
